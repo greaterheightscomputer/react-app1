c47edadf4b4a834bdb7b3bc952a0d1fbf14090ed
greaterheightscomputer <greaterheightscomputer@gmail.com>
2019-11-30 11:45:32 +0100

    rearrange app.js

--- src/app.js
+++ src/app.js
@@ -21,8 +21,10 @@ const jsx = (
     </Provider>
     );
 
-ReactDOM.render(<p>Loading...</p>, document.getElementById('app'));
+// ReactDOM.render(<p>Loading...</p>, document.getElementById('app'));
 
-store.dispatch(startSetExpenses()).then(() => {
+// store.dispatch(startSetExpenses()).then(() => {
+//     ReactDOM.render(jsx, document.getElementById('app'));    
+// });
+
     ReactDOM.render(jsx, document.getElementById('app'));    
-});
