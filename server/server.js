e3ef2c2ffb299ec12d409b84bc2def4352c43802
greaterheightscomputer <greaterheightscomputer@gmail.com>
2019-12-01 01:40:47 +0100

    Setup production build build and server

--- server/server.js
+++ server/server.js
@@ -1,13 +1,13 @@
 const path = require('path');
 const express = require('express'); //node way of importing third party library
 const app = express();
-const publicPath = path.join(__dirname, '..', 'public');  //the path to serve up our public asset
+const publicPath = path.join(__dirname, '..', 'public');  //'..' means go out the server folder inorder to find the path to serve up our public asset
 const port = process.env.PORT || 3000;   //heroku use this process.env.PORT to generate dynamic port number and server up the public asset to browser while port 3000 is to server up the public asset on the local machine
 
 app.use(express.static(publicPath));
 
-app.get('*', (req, res) => {
-    res.sendFile(path.join(publicPath, 'index.html'));  //when a request is made by user the index.html page is return inorder to response to user responser and to avoid error 404 
+app.get('*', (req, res) => {   //'*' represent all the pages or route in the application
+    res.sendFile(path.join(publicPath, 'index.html'));  //if the page a user want is not in the public folder just return the index instead of return error 404. When you refresh any page its return error 404 but with this setting its will return index page instead.   
 });
 
 app.listen(port, () => {
